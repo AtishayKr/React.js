161c1bb053798ea499a06ecd4c1c047b712ff236
AtishayKr <atishay1950@gmail.com>
2023-12-24 17:59:32 +0530

    fixed some bug

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   return (
     <>
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
-      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <h1 className='text-white text-center my-3'>Random Key Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
         type='text'
@@ -57,7 +57,7 @@ function App() {
             <input 
             type='range'
             min={8}
-            max={50}
+            max={35}
             value={length}
             className='cursor-pointer'
             onChange={(e) => {setLength(e.target.value)}}
